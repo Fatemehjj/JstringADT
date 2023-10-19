998e83e56ef67f43aa49b09a6985e3417e9af1ae
fatemehjj <joolafatemeh652@gmail.com>
2023-10-19 12:06:00 +0330

    set new tests

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,12 @@
 public class Test {
     public static void main(String[] args) {
     Jmethods jstring = new Jstring();
-    String str = "ALGORITHM";
-        System.out.println(jstring.size(str));
-        System.out.println(jstring.reverse(str));
-        System.out.println(jstring.firstChar(str));
-        System.out.println(jstring.lastChar(str));
-        System.out.println(jstring.isBalanced(str));
+    jstring.add("ALGORITHM");
+    jstring.add("DATA");
+        System.out.println(jstring.getAll());
+        System.out.println(jstring.size());
+        System.out.println(jstring.reverse());
+        System.out.println(jstring.firstStr());
+        System.out.println(jstring.lastStr());
     }
 }
